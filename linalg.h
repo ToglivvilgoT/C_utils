85de9c52aab538821e5a139b8a6ac01f641d1624
Morgan <mnordberg01@gmail.com>
2026-08-01 12:30:24 +0200

    vector lerp

--- linalg.h
+++ linalg.h
@@ -3,63 +3,61 @@
 #include "math.h"
 
 
-typedef struct 
-{
-	union {
-		struct 
-		{ float x, y, z; };
-		float v[3];
-	};
+typedef struct {
+  union {
+    struct {
+      float x, y, z;
+    };
+    float v[3];
+  };
 } Vec3;
 
-typedef struct 
-{
-	union {
-		struct 
-		{ float x, y, z, w; };
-		float v[4];
-	};
+typedef struct {
+  union {
+    struct {
+      float x, y, z, w;
+    };
+    float v[4];
+  };
 } Vec4;
 
 Vec3 vec3(Vec4);
 Vec4 vec4(Vec3);
 Vec3 new_vec3(float, float, float);
-Vec4 new_vec4(float,float,float,float);
+Vec4 new_vec4(float, float, float, float);
 
-Vec3 vec3_add_val(Vec3, float);
-Vec3 vec3_sub_val(Vec3, float);
-Vec3 vec3_mult_val(Vec3, float);
-Vec3 vec3_div_val(Vec3, float);
-Vec3 vec3_neg(Vec3);
-Vec3 vec3_add(Vec3, Vec3);
-Vec3 vec3_sub(Vec3, Vec3);
-Vec3 vec3_norm(Vec3);
+Vec3  vec3_add_val(Vec3, float);
+Vec3  vec3_sub_val(Vec3, float);
+Vec3  vec3_mult_val(Vec3, float);
+Vec3  vec3_div_val(Vec3, float);
+Vec3  vec3_neg(Vec3);
+Vec3  vec3_add(Vec3, Vec3);
+Vec3  vec3_sub(Vec3, Vec3);
+Vec3  vec3_norm(Vec3);
 float vec3_length(Vec3);
 float vec3_dist(Vec3, Vec3);
-Vec3 cross(Vec3, Vec3);
+Vec3  cross(Vec3, Vec3);
 float dot3(Vec3, Vec3);
 
-Vec4 vec4_add_val(Vec4, float);
-Vec4 vec4_sub_val(Vec4, float);
-Vec4 vec4_mult_val(Vec4, float);
-Vec4 vec4_div_val(Vec4, float);
-Vec4 vec4_neg(Vec4);
-Vec4 vec4_add(Vec4, Vec4);
-Vec4 vec4_sub(Vec4, Vec4);
-Vec4 vec4_norm(Vec4);
+Vec4  vec4_add_val(Vec4, float);
+Vec4  vec4_sub_val(Vec4, float);
+Vec4  vec4_mult_val(Vec4, float);
+Vec4  vec4_div_val(Vec4, float);
+Vec4  vec4_neg(Vec4);
+Vec4  vec4_add(Vec4, Vec4);
+Vec4  vec4_sub(Vec4, Vec4);
+Vec4  vec4_norm(Vec4);
 float vec4_length(Vec4);
 float vec4_dist(Vec4, Vec4);
 float dot4(Vec4, Vec4);
+Vec4  vec4_lerp(Vec4, Vec4, float);
 
-
-typedef struct
-{
-	float m[9]; 
+typedef struct {
+  float m[9];
 } Mat3;
 
-typedef struct
-{
-	float m[16]; 
+typedef struct {
+  float m[16];
 } Mat4;
 
 Mat3 mat3_add(Mat3, Mat3);
